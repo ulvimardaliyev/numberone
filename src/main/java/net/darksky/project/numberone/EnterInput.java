04079fb1d704f73939f9683ffc742c3f819a168d
Ulvi Mardaliyev <m.ulvi@ultra.local>
2018-08-21 16:45:54 +0400

    all codes

---
+++ src/main/java/net/darksky/project/numberone/EnterInput.java
@@ -0,0 +1,28 @@
+package net.darksky.project.numberone;
+
+import java.util.Scanner;
+
+public class EnterInput {
+	private Scanner scanner = new Scanner(System.in);
+	private double latitude;
+	private double longitude;
+
+	public double getLatitude() {
+		this.latitude = scanner.nextDouble();
+		if (latitude < 90 && latitude > 90) {
+			throw new IllegalArgumentException("provide a latitude between 0 and 90");
+		} else {
+			return this.latitude;
+		}
+	}
+
+	public double getLongitude() {
+		this.longitude = scanner.nextDouble();
+		if (longitude < 90 && longitude > 90) {
+			throw new IllegalArgumentException("provide a latitude between 0 and 90");
+		} else {
+			return this.longitude;
+		}
+	}
+
+}
