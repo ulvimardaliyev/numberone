49d1c1686277b475d9bbcd3c25e55c34e4db509f
Ulvi Mardaliyev <m.ulvi@ultra.local>
2018-08-24 16:36:02 +0400

    added .settings to .gitignore

--- src/main/java/net/darksky/project/numberone/App.java
+++ src/main/java/net/darksky/project/numberone/App.java
@@ -15,37 +15,26 @@ import tk.plogitech.darksky.forecast.model.Forecast;
 public class App {
 	public static void main(String[] args) throws IOException, ForecastException {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Welcome to my Forecast application.\r\n" + 
-				"Please provide your current latitude and longtitude:");
-		
-		
-		
-		
-		
-		
-		//ReadAFile readAFile = new ReadAFile("welcome.properties");
+		System.out.println(
+				"Welcome to my Forecast application.\r\n" + "Please provide your current latitude and longtitude:");
+		// ReadAFile readAFile = new ReadAFile("welcome.properties");
 		CreateForecast createForecast = new CreateForecast();
 		EnterInput enterInput = new EnterInput();
-		
-		//readAFile.readWords();
+
+		// readAFile.readWords();
 		createForecast.setLatitude(enterInput.getLatitude());
 		createForecast.setLongitude(enterInput.getLongitude());
 		createForecast.setAPIKey("462aefd9f99c6cda185c40262313a9d7");
-		
-		
-		ForecastRequest forecastRequest = new ForecastRequestBuilder()
-				.key(createForecast.getAPIKey())
-				.location(createForecast.getGeoCoordinates())
-				.language(Language.az)
-				.build();
+
+		ForecastRequest forecastRequest = new ForecastRequestBuilder().key(createForecast.getAPIKey())
+				.location(createForecast.getGeoCoordinates()).language(Language.ru).build();
 		DarkSkyJacksonClient darkSkyJacksonClient = new DarkSkyJacksonClient();
 		Forecast forecast = darkSkyJacksonClient.forecast(forecastRequest);
 		System.out.println("Now, you will get the forecast");
-		//readAFile.readRemainingWords();
-		System.out.println("the current weather is: "+forecast.getCurrently().getSummary());
-		System.out.println("daily weather is: " +forecast.getDaily().getSummary());
-		System.out.println("hourly weather is: "+forecast.getHourly().getSummary());
-		
-		
+		// readAFile.readRemainingWords();
+		System.out.println("the current weather is: " + forecast.getCurrently().getSummary());
+		System.out.println("daily weather is: " + forecast.getDaily().getSummary());
+		System.out.println("hourly weather is: " + forecast.getHourly().getSummary());
+
 	}
 }
